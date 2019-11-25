a6d423e1e9ccd13cec2f0f013cc5e115606917c9
wangw571 <wang_w571@hotmail.com>
2019-11-25 00:22:21 -0500

    Added 3D Object based GUI

---
+++ CameraTrackingMod Project/src/Camera_Tracking_Mod/ButtonBalls.cs
@@ -0,0 +1,361 @@
+﻿using System;
+using System.Collections.Generic;
+using Modding;
+using UnityEngine;
+
+
+namespace forceUnityTools
+{
+    public static class ButtonBalls 
+    {
+        //public ButtonBalls()
+        //{
+        //    GameObject go = new GameObject();
+        //}
+        static ChewChew yumyumyum;
+        public delegate void UpdateEvent();
+        public static Vector3 RatioApply(Rect Screen, Vector3 Ratio)
+        {
+            return new Vector3(Screen.width * Ratio.x / 100, Screen.height * Ratio.y / 100, Ratio.z);
+        }
+
+        static ClickButtonBall ShowUpAlll;
+        static KeyAssignButtonBall TriggerSetter;
+        static KeyAssignButtonBall TriggerOffSetter;
+        static SliderButtonBall ActivationRange;
+
+        public static void INNIT(ChewChew yumyum)
+        {
+            GameObject broadCast = new GameObject("MousePosBroadCaster");
+            MouseRayBroadCaster 广播者 = broadCast.AddComponent<MouseRayBroadCaster>();
+            yumyumyum = yumyum;
+            GameObject ShowUpAll = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+            StatMaster.DontDestroyOnLoad(ShowUpAll);
+            ShowUpAlll = ShowUpAll.AddComponent<ClickButtonBall>();
+            UpdateEvent EUSUA = new UpdateEvent(ShowThemAll);
+            ShowUpAlll.SetUp(Color.red, Color.white, true, false, "Show Camera \n Tracking Menu", EUSUA, new Vector3(0, 40, 35), new Vector3(0, 40, -15 ));
+            ShowUpAlll.Show();
+            广播者.AddMeIn(ShowUpAlll);
+
+
+
+            GameObject SetTriggerKey = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            StatMaster.DontDestroyOnLoad(SetTriggerKey);
+            TriggerSetter = SetTriggerKey.AddComponent<KeyAssignButtonBall>();
+            UpdateEvent EUTS = new UpdateEvent(UpdateKey);
+            TriggerSetter.SetUp(Color.cyan, Color.red, KeyCode.RightAlt, "Key For \nTracking: {0}", EUTS, new Vector3(20, 40, 15), new Vector3(120, 120, -150));
+            广播者.AddMeIn(TriggerSetter);
+
+
+            GameObject SetOffKey = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            StatMaster.DontDestroyOnLoad(SetOffKey);
+            TriggerOffSetter = SetOffKey.AddComponent<KeyAssignButtonBall>();
+            UpdateEvent EUTOS = new UpdateEvent(UpdateOffKey);
+            TriggerOffSetter.SetUp(Color.cyan, Color.red, KeyCode.Mouse1, "Key For Disable\nTracking: {0}", EUTOS, new Vector3(40, 40, 15), new Vector3(120, 130, -150));
+            广播者.AddMeIn(TriggerOffSetter);
+
+
+
+            GameObject ActivationRangeBall = GameObject.CreatePrimitive(PrimitiveType.Capsule);
+            StatMaster.DontDestroyOnLoad(ActivationRangeBall);
+            ActivationRange = ActivationRangeBall.AddComponent<SliderButtonBall>();
+            UpdateEvent EUAR = new UpdateEvent(UpdateActivationRange);
+            ActivationRange.SetUp(1, 100, 35, 0.1f, "Track when target \nat {0}% on the screen", EUAR, new Vector3(15, 60, 25), new Vector3(120, 140, -150));
+            广播者.AddMeIn(ActivationRange);
+        }
+
+        static void ShowThemAll()
+        {
+            if (ShowUpAlll.SwitchBool)
+            {
+                TriggerSetter.Show();
+                ActivationRange.Show();
+                TriggerOffSetter.Show();
+            }
+            else
+            {
+                TriggerSetter.Hide();
+                ActivationRange.Hide();
+                TriggerOffSetter.Hide();
+            }
+        }
+
+        static void UpdateKey()
+        {
+            yumyumyum.activation = TriggerSetter.toAssign;
+        }
+        static void UpdateOffKey()
+        {
+            yumyumyum.triggerOffKey = TriggerOffSetter.toAssign;
+        }
+
+        static void UpdateActivationRange()
+        {
+            yumyumyum.ActivationRange = ActivationRange.Value / 100;
+        }
+    }
+
+    public abstract class AbsButtonBall : MonoBehaviour
+    {
+        protected bool ShowUp;
+        public string Description;
+        public Vector3 ShowLocationRatio = new Vector2(11.4f, 51.4f);
+        public Vector3 HideLocationRatio = new Vector2(1919, 810);
+        public ButtonBalls.UpdateEvent updateEvent;
+        public GameObject myText;
+        protected TextMesh myTextMesh;
+        protected Renderer MeeRee;
+        protected Collider Mee;
+        public abstract void MouseRelatedUpdate(RaycastHit[] hits);
+        protected abstract void MyUpdate();
+        void MyStart()
+        {
+            MeeRee = this.GetComponent<Renderer>();
+            //Texture ttx = MeeRee.material.mainTexture;
+            //MeeRee.material.shader = Shader.Find("FX/Glass/Stained BumpDistort");
+            //MeeRee.material.mainTexture = ttx;
+            MeeRee.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.Off;
+            Mee = this.GetComponent<Collider>();
+            Mee.isTrigger = true;
+        }
+        public void Show()
+        {
+            ShowUp = true;
+        }
+        public void Hide()
+        {
+            ShowUp = false;
+        }
+        void Start()
+        {
+            myText = new GameObject("myDescription");
+            myText.transform.parent = this.transform;
+            myText.transform.localPosition = Vector3.zero;
+            myText.transform.localScale = new Vector3(-0.05f, 0.05f, 0.05f);
+            myTextMesh = myText.AddComponent<TextMesh>();
+            myTextMesh.fontSize = 72;
+            myTextMesh.color = Color.black;
+            myTextMesh.font = (Font)Resources.GetBuiltinResource(typeof(Font), "Arial.ttf"); ;
+            myText.AddComponent<AutoLookAt>();
+            MyStart();
+        }
+        void Update()
+        {
+            //this.transform.position = Vector3.Lerp(
+            //    this.transform.position,
+            //    Camera.main.ScreenToWorldPoint(ButtonBalls.RatioApply(Camera.main.pixelRect, ShowUp ? ShowLocationRatio : HideLocationRatio)),
+            //    1f
+            //    );
+
+            bool isBuildingMode = ((StatMaster.SimulationState == SimulationState.SpectatorMode) ||
+                (StatMaster.SimulationState == SimulationState.BuildMode) ||
+                (StatMaster.SimulationState == SimulationState.BuildModeGlobalSim) ||
+                (StatMaster.SimulationState == SimulationState.BuildModeGlobalSimRemoteLocalSim) ||
+                (StatMaster.SimulationState == SimulationState.BuildModeRemoteLocalSim));
+
+            this.transform.position = Vector3.Lerp(this.transform.position, 
+                Camera.main.ScreenToWorldPoint(ButtonBalls.RatioApply(Camera.main.pixelRect, ShowUp && isBuildingMode ? ShowLocationRatio : HideLocationRatio)), 0.1f);
+
+            this.transform.localScale = Vector3.Lerp(this.transform.localScale, ShowUp && isBuildingMode ? Vector3.one : Vector3.zero, 0.1f);
+            MyUpdate();
+        }
+        void FixedUpdate()
+        {
+            bool isBuildingMode = ((StatMaster.SimulationState == SimulationState.SpectatorMode) ||
+                (StatMaster.SimulationState == SimulationState.BuildMode) ||
+                (StatMaster.SimulationState == SimulationState.BuildModeGlobalSim) ||
+                (StatMaster.SimulationState == SimulationState.BuildModeGlobalSimRemoteLocalSim) ||
+                (StatMaster.SimulationState == SimulationState.BuildModeRemoteLocalSim));
+            this.transform.position = Vector3.Lerp(this.transform.position,
+                 Camera.main.ScreenToWorldPoint(ButtonBalls.RatioApply(Camera.main.pixelRect, ShowUp && isBuildingMode ? ShowLocationRatio : HideLocationRatio)), 0.1f);
+            this.transform.localScale = Vector3.Lerp(this.transform.localScale, ShowUp && isBuildingMode ? Vector3.one : Vector3.zero, 0.1f);
+        }
+    }
+    public class ClickButtonBall : AbsButtonBall
+    {
+        public Color OnColor;
+        public Color OffColor;
+        public bool isSwitch;
+        public bool SwitchBool;
+
+        public override void MouseRelatedUpdate(RaycastHit[] hits)
+        {
+
+            foreach (RaycastHit hitt in hits)
+            {
+                if (hitt.collider == Mee)
+                {
+                    MeeRee.material.color = OnColor;
+                    if (Input.GetKeyDown(KeyCode.Mouse0))
+                    {
+                        if (isSwitch)
+                        {
+                            SwitchBool = !SwitchBool;
+                        }
+                        updateEvent();
+                        
+                    }
+
+                    break;
+                }
+                else
+                {
+                    MeeRee.material.color = OffColor;                    
+                }
+            }
+            if (isSwitch)
+            {
+                MeeRee.material.color = SwitchBool ? OnColor : OffColor;
+            }
+        }
+
+        public void SetUp(Color OnColorR, Color AwayColorR, bool isSwitchh, bool defSwitchBool, String text, ButtonBalls.UpdateEvent toDo, Vector3 ShowRatio, Vector3 HideRatio)
+        {
+            OnColor = OnColorR;
+            OffColor = AwayColorR;
+            isSwitch = isSwitchh;
+            SwitchBool = defSwitchBool;
+            Description = text;
+            updateEvent = toDo;
+            ShowLocationRatio = ShowRatio;
+            HideLocationRatio = HideRatio;
+        }
+
+        protected override void MyUpdate() 
+        {
+            myTextMesh.text = string.Format(Description, SwitchBool);
+        }
+
+    }
+
+    public class KeyAssignButtonBall : AbsButtonBall
+    {
+        public Color OnColor;
+        public Color AwayColor;
+        public KeyCode toAssign;
+        
+
+        public void SetUp(Color OnColorR, Color AwayColorR, KeyCode DefaultKey, String text, ButtonBalls.UpdateEvent toDo, Vector3 ShowRatio, Vector3 HideRatio)
+        {
+            OnColor = OnColorR;
+            AwayColor = AwayColorR;
+            toAssign = DefaultKey;
+            Description = text;
+            updateEvent = toDo;
+            ShowLocationRatio = ShowRatio;
+            ShowLocationRatio.z = 25;
+            HideLocationRatio = HideRatio;
+            HideLocationRatio.z = -1;
+        }
+
+        public override void MouseRelatedUpdate(RaycastHit[] hits)
+        {
+            foreach(RaycastHit hitt in hits)
+            {
+                if (hitt.collider == Mee)
+                {
+                    MeeRee.material.color = OnColor;
+                    if (Input.anyKey)
+                        toAssign = Event.current.keyCode == KeyCode.None? toAssign : Event.current.keyCode;
+
+                    updateEvent();
+                    break;
+                }
+                else
+                {
+                    MeeRee.material.color = AwayColor;
+                }
+            }
+        }
+
+        protected override void MyUpdate() 
+        {
+            myTextMesh.text = string.Format(Description, toAssign.ToString());
+        }
+    }
+
+    public class SliderButtonBall : AbsButtonBall
+    {
+        bool MouseDraggingPrev = false;
+        bool OnMe = false;
+        Vector3 MousePosPrev = Vector3.zero;
+        public float Min;
+        public float Max;
+        public float Value;
+        public float DragRatio;
+
+        public void SetUp(float min, float max, float defValue, float DragRatioo, String text, ButtonBalls.UpdateEvent toDo, Vector3 ShowRatio, Vector3 HideRatio)
+        {
+            Min = min;
+            Max = max;
+            Value = defValue;
+            DragRatio = DragRatioo;
+            Description = text;
+            updateEvent = toDo;
+            ShowLocationRatio = ShowRatio;
+            HideLocationRatio = HideRatio;
+            this.gameObject.AddComponent<AutoLookAt>();
+        }
+        public override void MouseRelatedUpdate(RaycastHit[] hits)
+        {
+            foreach (RaycastHit hitt in hits)
+            {
+                OnMe = hitt.collider == Mee;
+                if (OnMe) break;
+            }
+        }
+
+        protected override void MyUpdate()
+        {
+            if (Input.GetKey(KeyCode.Mouse0) && (OnMe|| MouseDraggingPrev))
+            {
+                if (MouseDraggingPrev)
+                {
+                    float diff = Vector3.Dot((Input.mousePosition - MousePosPrev), new Vector3(1, 1, 0));
+                    Value += diff* DragRatio;
+                    Value = Mathf.Clamp(Value, Min, Max);
+                }
+                MousePosPrev = Input.mousePosition;
+                MouseDraggingPrev = true;
+
+                updateEvent();
+            }
+            else
+            {
+                MouseDraggingPrev = false;
+            }
+            myTextMesh.text = string.Format(Description, Value);
+        }
+
+    }
+
+
+
+    public class MouseRayBroadCaster : MonoBehaviour
+    {
+        public List<AbsButtonBall> buttonBalls = new List<AbsButtonBall>();
+        public void AddMeIn(AbsButtonBall oneButtonBall)
+        {
+            buttonBalls.Add(oneButtonBall);
+        }
+
+        void Update()
+        {
+            RaycastHit[] hits = Physics.RaycastAll(Camera.main.ScreenPointToRay(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 720)));
+            foreach(AbsButtonBall bb in buttonBalls)
+            {
+                bb.MouseRelatedUpdate(hits);
+            }
+        }
+    }
+
+    public class AutoLookAt:MonoBehaviour
+    {
+        void Update()
+        {
+            this.transform.LookAt(Camera.main.transform);
+        }
+    }
+
+
+}
--- CameraTrackingMod Project/src/Camera_Tracking_Mod/Mod.cs
+++ CameraTrackingMod Project/src/Camera_Tracking_Mod/Mod.cs
@@ -11,8 +11,9 @@ namespace forceUnityTools
         {
 
             GameObject go = new GameObject("Camera Tracking Mod");
-            go.AddComponent<ChewChew>();
+            ChewChew chw = go.AddComponent<ChewChew>();
             StatMaster.DontDestroyOnLoad(go);
+            ButtonBalls.INNIT(chw);
         }
     }
 
@@ -27,13 +28,19 @@ namespace forceUnityTools
         public Transform CurrentTarget;
         public Vector3 CurrentTargetPosition = Vector3.zero;
         public Vector3 MidPosition;
-        public Vector3 DisplayerTheEuler = Vector3.zero;
+        public bool UseMidPosition = false;
         private List<Transform> targetList = new List<Transform>();
         private int currentTargetIndex = 0;
         GameObject testBall;
         public int PosNeg = -1;
 
-        public int Additional360 = 0;
+        public float ActivationRange = 0.35f;
+
+        public KeyCode triggerOffKey = KeyCode.Mouse1;
+        public KeyCode activation = KeyCode.RightAlt;
+
+        public int Additional360LR = 0;
+        public int Additional360UD = 0;
         bool locateMouseOrbit()
         {
             if (theMouseOrbit == null || theMouseOrbit.gameObject == null)
@@ -47,7 +54,7 @@ namespace forceUnityTools
                         defaultFoV = mouseOrbitMainCam.fieldOfView;
                     }
                     theMouseOrbit.focusLerpSmooth = 1145141919;
-                    theMouseOrbit.yMaxLimit = 80;
+                    //theMouseOrbit.yMaxLimit = 893893;
                     //theMouseOrbit.yMinLimit = -893893;
                     //theMouseOrbit.xSpeed = 99999;
                     //theMouseOrbit.ySpeed = 99999;
@@ -63,17 +70,21 @@ namespace forceUnityTools
         {
             //if (testBall == null) { testBall = GameObject.CreatePrimitive(PrimitiveType.Sphere); testBall.transform.position = new Vector3(0, 5, 0); }
             if (StatMaster.isMP) { MPTargetAdd(); } else { SPTargetAdd(); }
-            if (!locateMouseOrbit()) { return; } 
-            if (Enabllled) Track();
-            DisplayerTheEuler = theMouseOrbit.transform.eulerAngles;
-            if (Input.GetKey(KeyCode.Mouse1))
+            if (!locateMouseOrbit()) { return; }
+            if (Enabllled)
+            {
+                Track();
+            }
+            else { mouseOrbitMainCam.fieldOfView = Mathf.Lerp(mouseOrbitMainCam.fieldOfView, defaultFoV, 0.025f); }
+            if (Input.GetKey(triggerOffKey) || Input.GetKey(KeyCode.F1))
             {
                 Enabllled = false;
                 theMouseOrbit.xSpeed = 250;
                 theMouseOrbit.ySpeed = 120;
-                mouseOrbitMainCam.fieldOfView = defaultFoV;
+                Additional360UD = 0;
+                Additional360LR = 0;
             }
-            if (Input.GetKeyDown(KeyCode.RightAlt))
+            if (Input.GetKeyDown(activation))
             {
                 if (Enabllled == true)
                 {
@@ -83,6 +94,14 @@ namespace forceUnityTools
                 //CurrentTarget = testBall.transform;
                 Enabllled = true;
             };
+
+            //mouseOrbitMainCam.transform.eulerAngles = Vector3.Lerp(
+            //    mouseOrbitMainCam.transform.eulerAngles,
+            //        new Vector3(
+            //            mouseOrbitMainCam.transform.eulerAngles.x,
+            //            mouseOrbitMainCam.transform.eulerAngles.y,
+            //            mouseOrbitMainCam.transform.eulerAngles.y > 180 || mouseOrbitMainCam.transform.eulerAngles.y < -180 ? 0 : 180
+            //            ), 0.2f);
         }
 
         void MPTargetAdd()
@@ -119,7 +138,7 @@ namespace forceUnityTools
             float pixelWidthF = (float)mouseOrbitMainCam.pixelWidth;
             float pixelHeightF = (float)mouseOrbitMainCam.pixelHeight;
 
-            MidPosition = ((CurrentTargetPosition - theMouseOrbit.target.position) / 2) + theMouseOrbit.target.position;
+            MidPosition = UseMidPosition? ((CurrentTargetPosition - theMouseOrbit.target.position) / 2) + theMouseOrbit.target.position: CurrentTargetPosition;
 
 
             Vector3 targetPositionOnScreen = getCenterXYOfScreen(CurrentTargetPosition);
@@ -128,47 +147,64 @@ namespace forceUnityTools
             float WidthRatio = Mathf.Abs(targetPositionOnScreen.x) / (pixelWidthF / 2);
             float HeightRatio = Mathf.Abs(targetPositionOnScreen.y) / (pixelHeightF / 2);
             if (targetPositionOnScreen.z < 0
-                || WidthRatio > 0.5f
-                || HeightRatio > 0.5f
+                || WidthRatio > ActivationRange * 2
+                || HeightRatio > ActivationRange * 2
                 || !CurrentTarget.gameObject.activeSelf
                 )
             {
-                mouseOrbitMainCam.fieldOfView = Mathf.Lerp(mouseOrbitMainCam.fieldOfView, 170, 0.004f * (CurrentTarget.gameObject.activeSelf?(WidthRatio + HeightRatio):12));
+                //BesiegeConsoleController.ShowMessage(string.Format("Out {0}", (0.004f * (CurrentTarget.gameObject.activeSelf ? (WidthRatio + HeightRatio) * (0.35f / ActivationRange) : 5))));
+                mouseOrbitMainCam.fieldOfView = Mathf.Lerp(mouseOrbitMainCam.fieldOfView, CurrentTarget.gameObject.activeSelf ? defaultFoV * 2:170, 0.0005f * (WidthRatio + HeightRatio) / (1*ActivationRange));
             }
-            if (targetPositionOnScreen.z < 0
-                || inRange(WidthRatio, 0.5f, 0.35f)
-                || inRange(HeightRatio, 0.5f, 0.35f)
+            else if (targetPositionOnScreen.z < 0
+                || inRange(WidthRatio, ActivationRange * 2, ActivationRange)
+                || inRange(HeightRatio, ActivationRange * 2, ActivationRange)
                 )
             {
-                mouseOrbitMainCam.fieldOfView = Mathf.Lerp(mouseOrbitMainCam.fieldOfView, defaultFoV, 0.0125f * (WidthRatio + HeightRatio));
-            }
+                //BesiegeConsoleController.ShowMessage(string.Format("Between {0}", 0.0005f * (WidthRatio + HeightRatio) * (0.35f / ActivationRange)));
 
-            mouseOrbitMainCam.fieldOfView = Mathf.Lerp(mouseOrbitMainCam.fieldOfView, defaultFoV, 0.0125f * (WidthRatio + HeightRatio));
+                mouseOrbitMainCam.fieldOfView = Mathf.Lerp(mouseOrbitMainCam.fieldOfView, defaultFoV, 0.01f * (WidthRatio + HeightRatio) / (0.5f*ActivationRange));
+            }
+            else
+            {
+                //BesiegeConsoleController.ShowMessage(string.Format("InSide {0}", 0.0025f * (2 / (WidthRatio + HeightRatio)) * (0.35f / ActivationRange)));
+
+                mouseOrbitMainCam.fieldOfView = Mathf.Lerp(mouseOrbitMainCam.fieldOfView, defaultFoV, 0.02f * (2 / (WidthRatio + HeightRatio)) / (0.75f*ActivationRange));
+            }
 
 
 
             Vector3 MidPositionOnScreen = getCenterXYOfScreen(CurrentTargetPosition);
-            if (WidthRatio > 0.35f)
+            if (WidthRatio > ActivationRange)
             {
                 this.transform.position = theMouseOrbit.transform.position;
                 this.transform.LookAt(MidPosition - this.transform.position);
                 float newY = this.transform.eulerAngles.y;
-                while (Mathf.Abs(newY - (theMouseOrbit.x - Additional360)) > 180)
+                while (Mathf.Abs(newY - (theMouseOrbit.x - Additional360LR)) > 180)
                 {
                     //BesiegeConsoleController.ShowMessage(string.Format("{0} -> {1}, {2}", theMouseOrbit.x, theMouseOrbit.x + Additional360, newY));
-                    if (newY > 180) Additional360 -= 360;
-                    else Additional360 += 360;
+                    if (newY > 180) Additional360LR -= 360;
+                    else Additional360LR += 360;
                 }
-                theMouseOrbit.x = Mathf.Lerp(theMouseOrbit.x, newY + Additional360, 0.05f * WidthRatio); ;
-
+                theMouseOrbit.x = Mathf.Lerp(theMouseOrbit.x, newY + Additional360LR, CurrentTarget.gameObject.activeSelf ? 0.05f * WidthRatio / (4*ActivationRange) : 0.8f); 
 
                 //theMouseOrbit.x = Mathf.Lerp(theMouseOrbit.x, theMouseOrbit.x + Mathf.Sign(MidPositionOnScreen.x) * ((float)pixelWidthF) / (1000), 0.25f);
                 //theMouseOrbit.y = Mathf.Lerp(theMouseOrbit.y, theMouseOrbit.y + pixelHeightF / 4500 * (CurrentTargetPosition.y <= theMouseOrbit.target.transform.position.y ? -1 : 1), 0.8f);
 
             }
-                
-            if (HeightRatio > 0.35f)
-                theMouseOrbit.y = Mathf.Lerp(theMouseOrbit.y, this.transform.eulerAngles.x, 0.05f * HeightRatio);
+
+            if (HeightRatio > ActivationRange)
+            {
+                this.transform.position = theMouseOrbit.transform.position;
+                this.transform.LookAt(MidPosition - this.transform.position);
+                float newX = this.transform.eulerAngles.x;
+                while (Mathf.Abs(newX - (theMouseOrbit.y - Additional360UD)) > 180)
+                {
+                    //BesiegeConsoleController.ShowMessage(string.Format("{0} -> {1}, {2}", theMouseOrbit.x, theMouseOrbit.x + Additional360, newY));
+                    if (newX > 180) Additional360UD -= 360;
+                    else Additional360UD += 360;
+                }
+                theMouseOrbit.y = Mathf.Lerp(theMouseOrbit.y, newX + Additional360UD, 0.05f * HeightRatio/ (4 * ActivationRange)); 
+            }
             //else if (Mathf.Abs(MidPositionOnScreen.y) / (mouseOrbitMainCam.pixelHeight / 2) < 0.4f) theMouseOrbit.y += Math.Sign(MidPositionOnScreen.y) * -1;
 
 
